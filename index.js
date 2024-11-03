38ae58e9cea30da455501e9de6773c94728cfb9f
AlaaTaha <alaataha@192.168.1.9>
2024-11-02 23:07:39 -0700

    sssss3

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const cors = require('cors');
 const compression = require('compression');
+const TelegramBot = require('node-telegram-bot-api');
 
 dotenv.config({ path: 'config.env' });
 const ApiError = require('./utils/apiError');
@@ -12,6 +13,9 @@ const globalError = require('./middlewares/errorMiddleware');
 const dbConnection = require('./config/database');
 // Routes
 const mountRoutes = require('./routes');
+const token = '7733472718:AAEtJ8ob2MY2ICT7PXSoCgvPheA2UvgJB0Y';
+
+const bot = new TelegramBot(token, { polling: true });
 
 // Connect with db
 dbConnection();
@@ -38,11 +42,39 @@ if (process.env.NODE_ENV === 'development') {
 // Mount Routes
 mountRoutes(app);
 
+app.get('/', () => {
+  console.log('asdasdas');
+  //res.send("asdasasdasd");
+})
 
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  const userName = msg.from.first_name || "مستخدم"; // الحصول على اسم المستخدم
 
+  const welcomeMessage = `مرحبًا ${userName}، أهلاً بك في البوت الخاص بنا!
+  الأوامر المتاحة:
+  1. /tiktok - للترحيب بالمستخدم
+  2. /facebook - لعرض قائمة الأوامر
+  3. /whatsapp - لعرض معلومات عن البوت
+  `;
+  bot.sendMessage(chatId, welcomeMessage);
+});
 
-
-
+bot.onText(/\/tiktok/, (msg) => {
+  const chatId = msg.chat.id;
+  const welcomeMessage = `https://www.tiktok.com/@alaa_taha_dev`;
+  bot.sendMessage(chatId, welcomeMessage);
+});
+bot.onText(/\/facebook/, (msg) => {
+  const chatId = msg.chat.id;
+  const welcomeMessage = `https://www.facebook.com/alaa.taha.71271466`;
+  bot.sendMessage(chatId, welcomeMessage);
+});
+bot.onText(/\/whatsapp/, (msg) => {
+  const chatId = msg.chat.id;
+  const welcomeMessage = `whatsapp://send?phone=+201111529018`;
+  bot.sendMessage(chatId, welcomeMessage);
+});
 app.all('*', (req, res, next) => {
   next(new ApiError(`Can't find this route: ${req.originalUrl}`, 400));
 });
--- services/testServices.js
+++ services/testServices.js
@@ -6,12 +6,13 @@ const ApiFeatures = require('../utils/apiFeatures');
 
 
 exports.getData=asyncHandler(async (req, res, next) => {
-res.send("asdasasdasd");
+    console.log('asdasdas'); 
+    res.send("asdasasdasd");
 })
 
 
 /*
 git add .
-git commit -m "sssss1"
+git commit -m "sssss2"
 git push -u origin main
 */
